9fa2907ea8d1f3cc9d24c2ce5e31c49b3ea8c5a4
hangoma qurbonbek <hangomajon@gmail.com>
2023-03-05 14:35:31 +0100

    controllers & models has been created

--- app/controllers/keys_controller.rb
+++ app/controllers/keys_controller.rb
@@ -3,45 +3,45 @@ class KeysController < ApplicationController
     @keys = Key.all
   end
 
-  # def show
-  #   @key = Key.find(params[:id])
-  # end
+  def show
+    @key = Key.find(params[:id])
+  end
 
-  # def new
-  #   @key = Key.new
-  # end
+  def new
+    @key = Key.new
+  end
 
-  # def create
-  #   @key = Key.new(key_params)
-  #   if @key.save
-  #     redirect_to @key
-  #   else
-  #     render :new
-  #   end
-  # end
+  def create
+    @key = Key.new(key_params)
+    if @key.save
+      redirect_to @key
+    else
+      render :new
+    end
+  end
 
-  # def edit
-  #   @key = Key.find(params[:id])
-  # end
+  def edit
+    @key = Key.find(params[:id])
+  end
 
-  # def update
-  #   @key = Key.find(params[:id])
-  #   if @key.update(key_params)
-  #     redirect_to @key
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    @key = Key.find(params[:id])
+    if @key.update(key_params)
+      redirect_to @key
+    else
+      render :edit
+    end
+  end
 
-  # def destroy
-  #   @key = Key.find(params[:id])
-  #   @key.destroy
-  #   redirect_to keys_path
-  # end
+  def destroy
+    @key = Key.find(params[:id])
+    @key.destroy
+    redirect_to keys_path
+  end
 
-  # private
+  private
 
-  # def key_params
-  #   params.require(:key).permit(:title, :description, :type_of_key, :property_id)
-  # end
+  def key_params
+    params.require(:key).permit(:title, :description, :type_of_key, :property_id)
+  end
 end
