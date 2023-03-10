6f389223e2be5eb4a7d90c0bd4ae7cb696e6a4bc
Tom Elsom <te28@hotmail.com>
2023-03-10 12:06:30 +0000

    working on somethig

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,4 +2,25 @@ class PropertiesController < ApplicationController
   def index
     @properties = Property.all
   end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+    @property.visible = true
+    @property.user = current_user
+    @property.save!
+    redirect_to property_path
+  end
+
+  def show
+    @property = Property.find(params[:id])
+  end
+
+  private
+    def property_params
+      params.require(:property).permit(:address, :city, :postcode, :move_in, :move_out, :visible)
+    end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,8 @@
 class Property < ApplicationRecord
   belongs_to :user
+<<<<<<< HEAD
   has_many :rooms
+=======
+  has_many :keys
+>>>>>>> 4d97319c43a7f9d726b78f6ac798ca5e3ae828ee
 end
