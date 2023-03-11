9a758238d5a0d881942e13f460373079d7d079fd
Hugo Voorheijen <h.voorheijen@gmail.com>
2023-03-11 17:33:00 +0100

    readingspart

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,9 @@ Rails.application.routes.draw do
     resources :keys
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :properties do
+    resources :keys
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
