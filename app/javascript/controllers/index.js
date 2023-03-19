ee2f118f7b7c64e6c8318b1d6dd3996176168577
Michaela Katzenmueller <m.katzenmueller@gmx.de>
2023-03-19 14:23:04 +0100

    Yes

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,19 +1,6 @@
 import { application } from "./application";
 import { definitionsFromContext } from "@stimulus/webpack-helpers";
 
-<<<<<<< HEAD
-import { application } from "./application"
-import { definitionsFromContext } from "@stimulus/webpack-helpers"
-
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
-import DamagePhotosController from "./damage_photos_controller"
-application.register("damage-photos", DamagePhotosController)
-
-const context = require.context(".", true, /\.js$/)
-application.load(definitionsFromContext(context))
-=======
 import HelloController from "./hello_controller";
 application.register("hello", HelloController);
 
@@ -33,6 +20,3 @@ application.register("insert-in-list", InsertInListController)
 
 const context = require.context(".", true, /\.js$/);
 application.load(definitionsFromContext(context));
-
-
->>>>>>> 3be49c4e905363e89e943dc22822096d73578db9
