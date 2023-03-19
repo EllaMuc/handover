be167dba8eda1760d41ee6735d66e5a1a57450ca
Michaela Katzenmueller <m.katzenmueller@gmx.de>
2023-03-19 13:40:43 +0100

    No

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,12 +3,13 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
-
-import DamagePhotosController from "./damage_photos_controller"
-application.register("damage-photos", DamagePhotosController)
+import { definitionsFromContext } from "@stimulus/webpack-helpers"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import InsertInListController from "./insert_in_list_controller"
-application.register("insert-in-list", InsertInListController)
+import DamagePhotosController from "./damage_photos_controller"
+application.register("damage-photos", DamagePhotosController)
+
+const context = require.context(".", true, /\.js$/)
+application.load(definitionsFromContext(context))
