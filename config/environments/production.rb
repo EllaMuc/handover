3d733964e114dc9efbbd38bea88ed325270b8a1f
tomelsom01 <te28@hotmail.com>
2023-03-25 14:57:16 +0000

    added keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "handover.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "mysterious-thicket-64838.herokuapp.com" }
 end
